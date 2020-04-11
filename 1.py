15a22b2f6dae458467ed0bc1da96990ba0e1b146
lNeverl <1723957192@qq.com>
2020-04-11 09:11:35 +0800

    [demo]

---
+++ 1.py
@@ -0,0 +1,18 @@
+import json
+import time
+while True:
+    f = open('c:\\cdr\\responseBody.txt', 'r', encoding='utf-8')
+    j = json.load(f)
+    #print(j['data']['options'])
+    if 'options' in j['data']:
+        lenth=len(j['data']['options'])
+        for i in range(0,lenth):
+            # print(j['data']['options'][i]['answer'])
+            answer = str(j['data']['options'][i]['answer'])
+            if answer == 'True':
+                print(j['data']['options'][i]['content'])
+        print('--------------------------------------------------')
+    if 'answer_content' in j['data']:
+        if 'stem' in j['data']:
+            print(j['data']['answer_content'])
+    time.sleep(4)
